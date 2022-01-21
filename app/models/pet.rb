02369e645f9b9c9aaf986a3a0484a91f8fd948d1
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-21 10:52:06 -0800

    BUGFIX: capitalize species includes validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,6 @@ class Pet < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true
   validates :species, presence: true
-  validates :species, inclusion: { in: %w(cat dog bunny),
+  validates :species, inclusion: { in: %w(Cat Dog Bunny),
     message: "%{value} is not a valid species" }
 end
