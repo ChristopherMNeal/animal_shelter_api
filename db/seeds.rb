53efefd315494586caab7c90a58866137dbd6e08
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-21 09:06:47 -0800

    add db seeds; add breed to pets table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_pets
+  end
+
+  def generate_pets
+    20.times do |i|
+      pet = Pet.create!(
+        name: Faker::Creature::Cat.name,
+        breed: Faker::Creature::Cat.breed,
+        species: "Cat"
+      )
+      puts "Pet #{i}: #{pet.name} is a #{pet.breed} #{pet.species}."
+    end
+    20.times do |i|
+      pet = Pet.create!(
+        name: Faker::Creature::Dog.name,
+        breed: Faker::Creature::Dog.breed,
+        species: "Dog"
+      )
+      puts "Pet #{i}: #{pet.name} is a #{pet.breed} #{pet.species}."
+    end
+  end
+end
+
+Seed.begin
