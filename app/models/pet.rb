c9118c0d4233f79260e0441ba431fe2ae609fc0b
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-21 13:53:44 -0800

    add titleize methods for all pets columns

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,20 @@ class Pet < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true
   validates :species, presence: true
-  validates :species, inclusion: { in: %w(Cat Dog Bunny),
+  validates :species, inclusion: { in: %w(Cat cat Dog dog Bunny bunny),
     message: "%{value} is not a valid species" }
   scope :search_name, -> (name_parameter) { where(name: name_parameter) }
+  before_save(:titleize_name)
+  before_save(:titleize_breed)
+  before_save(:titleize_species)
+  private
+    def titleize_name
+      self.name = self.name.titleize
+    end
+    def titleize_breed
+      self.breed = self.breed.titleize
+    end
+    def titleize_species
+      self.species = self.species.titleize
+    end
 end
