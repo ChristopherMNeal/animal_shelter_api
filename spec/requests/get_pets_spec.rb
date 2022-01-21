7fbddcfaf0aea1302d6b794308c12af7f2c10150
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-21 11:18:10 -0800

    add and pass get pets tests

---
+++ spec/requests/get_pets_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "get all pet listings route", :type => :request do
+  Pet.destroy_all
+  let!(:pets) { FactoryBot.create_list(:pet, 14)}
+
+  it 'returns the first 10 pets' do
+    get '/pets?page=1'
+    expect(JSON.parse(response.body).size).to eq(10)
+  end
+
+  it 'returns the last 4 pets' do
+    get '/pets?page=2'
+    expect(JSON.parse(response.body).size).to eq(4)
+  end
+  
+  it 'returns status code 200' do
+    get '/pets'
+    expect(response).to have_http_status(:success)
+  end
+end
