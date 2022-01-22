cff296ee0d924ad3d9b7d999c057d7f2b67a34f5
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-21 16:00:03 -0800

    add and pass length validation tests

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -8,7 +8,9 @@ describe Pet do
   it { should validate_presence_of :name }
   it { should validate_presence_of :breed }
   it { should validate_presence_of :species }
-  
+  it { should validate_length_of(:name).is_at_most(25) }
+  it { should validate_length_of(:breed).is_at_most(50) }
+
   it("titleizes the name of a pet") do
     expect(@pet.name()).to(eq("Hel Goddess Of Death"))
   end
